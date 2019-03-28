997f302c1af9c86274f989534529116759eef48a
Julian Claus <kontakt@julian-claus.de>
2019-03-28 08:44:58 +0100

    fix: manually define port

--- cmd/vue-go-example/main.go
+++ cmd/vue-go-example/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ndabAP/vue-go-example/internal/app/vue-go-example/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	r := routes.SetupRoutes()
 
-	r.Run(":3000")
+	r.Run(":" + port)
 }
